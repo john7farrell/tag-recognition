785d37350bf0799a007512f3417ff709940107fd
mat.xi <mat.xi@karabiner.tech>
2017-09-04 16:49:25 +0900

    add comment

--- flaskr/tag_recognition/preprocess.py
+++ flaskr/tag_recognition/preprocess.py
@@ -36,8 +36,10 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 
 def getResizedImage(img_name, img_obj=None,
                     maxHeight=600):
+    # if there is an image object, copy and then use it
     if img_obj is not None:
         img = img_obj.copy()
+    # if there is no img_obj, read image from file path and then use it
     else:
         img = cv2.imread(img_name)
     img_resized = resize(img, height=maxHeight, inter=cv2.INTER_LANCZOS4)
@@ -48,24 +50,38 @@ def getEnhancedImage(img_name, img_obj=None,
                      thrs_lo=145,
                      thrs_md=170,
                      thrs_hi=215):
+    # if there is an image object, copy and then use it
     if img_obj is not None:
         img = img_obj.copy()
+    # if there is no img_obj, read image from file path and then use it
     else:
         img = cv2.imread(img_name)
+
+    # convert to YCrCb color
     img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    # Y --- Luminance, to be adjusted
     img_Y = img_YCrCb[:, :, 0].astype(np.float)
+    # mean value of luminance(brightness)
     y_mean = np.mean(img_Y)
+
+    ### mapping y mean values to be three threshold(low, middle, high)
+    # if meanVal < threshold_low
     if y_mean < thrs_lo:
         img_Y *= thrs_lo / y_mean
         img_Y[img_Y>255] = 255
+    # if meanVal > threshold_high
     elif y_mean > thrs_hi:
         img_Y *= thrs_hi / y_mean
         img_Y[img_Y>255] = 255
+    # if thrs_lo < meanVal < thrs_hi
     else:
         img_Y *= thrs_md / y_mean
         img_Y[img_Y>255] = 255
+
+    # convert matrix type
     img_Y = img_Y.astype(np.uint8)
     img_YCrCb[:, :, 0] = img_Y
+    # convert back to be BGR color
     img_enhanced = cv2.cvtColor(img_YCrCb, cv2.COLOR_YCrCb2BGR)
     return img_enhanced
 
@@ -82,33 +98,48 @@ def getDeskewedImage(img_name, img_obj=None,
                      ang_min=-50,
                      ang_max=50,
                      ang_num=120):
+    # if there is an image object, copy and then use it
     if img_obj is not None:
         img = img_obj.copy()
+    # if there is no img_obj, read image from file path and then use it
     else:
         img = cv2.imread(img_name)
+    # convert to YCrCb color
     img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    # Y --- Luminance, to be used as grayscale image
     img_Y = img_YCrCb[:, :, 0].astype(np.float)
+
+
+    # possible angle range to be searched
     angle_li = np.linspace(ang_min, ang_max, ang_num)
+    # dict to save...
+    # angle:        angle, (float)
+    # sum_axis0:    sum of image matrics by axis-0, (np.array)
+    # s0_diff:      diff of sum_axis0, (np.array)
     s0_res = {'angle': [],
               'sum_axis0': [],
               's0_diff':[]}
     for angle in angle_li:
+        # rotate image by angle (zoom-in 1.3X)
         res = getRotatedImage(img_Y, angle, 1.3)
+        # calculate sum of image matrics in axis-0
         sum_axis0 = np.sum(res, axis=0).astype(np.float)
+        # save data
         s0_res['angle'].append(angle)
         s0_res['sum_axis0'].append(sum_axis0)
         s0_res['s0_diff'].append(np.diff(sum_axis0))
+    # convert to dataframe
     s0_res = pd.DataFrame(s0_res)
+    # get maximum of s0_diff for each angle
     s0_res['s0_diff_max'] = s0_res.s0_diff.apply(np.max)
+    # get position of s0_diff_max
     ang_idx = np.argmax(s0_res['s0_diff_max'])
     ang = angle_li[ang_idx]
     # print('rotate {}'.format(ang))
+
+    # if ang is 0 or 45 ~ ang_max, ang -> -ang
     if abs(ang_max)*(1-9e-5) > abs(ang) > 45:
         ang = -ang
         print('(fix)rotate {}'.format(ang))
     img_deskewed = getRotatedImage(img, ang)
     return img_deskewed
-#
-#
-# def getProcessedImage(img_name, img_obj=None):
-#
--- flaskr/test_tag_recognition.py
+++ flaskr/test_tag_recognition.py
@@ -15,11 +15,14 @@ from tag_recognition.preprocess \
 from tag_recognition.json_analyze import json_result
 from tag_recognition.gcv_api import gcv_result
 
+
 #%% config upload directory and apikey
 app_config = dict()
 app_config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/upload'
 with open('.apikey', 'r') as f:
     app_config['API_KEY'] = f.read()
+
+
 #%% set id rules
 id_rule_dict = {
     'BEAMS': [2, 2, 4, 3, 2, 2],
@@ -36,9 +39,13 @@ id_rule_dict = {
     'Soffitto': [10],
     'Unknown13': [13]
 }
+
+
 #%% get objective images
 file_dir = app_config['UPLOADED_PHOTOS_DEST']
 file_list = os.listdir(file_dir)
+
+
 ### add filter rules here to filter specific files !!!
 file_list = list(filter(lambda s: '.json' not in s and \
                         'maxHeight' not in s and \
@@ -52,10 +59,13 @@ res = {'Origin': [],
        'Part': [],
        'Filename': ''}
 
+
 #%% iteratively process
 cnt = 0
 for fullname in file_list:
     file_name, file_ext = os.path.splitext(fullname)
+
+
     ## preprocessing
     # 1. adjust brightness
     image_proc = getEnhancedImage(file_dir + '/' + file_name + file_ext)
@@ -64,27 +74,35 @@ for fullname in file_list:
     # 3. resize image
     image_proc = getResizedImage('', image_proc,
                                  maxHeight=900)
+
+
     ## save the processed image
     cv2.imwrite(file_dir + '/' + file_name + '_proc' + file_ext,
                 image_proc)
     del image_proc
     fname_proc = file_name + '_proc'
     fname_json = fname_proc + file_ext + '.json'
+
+
     # if no json file for the image uploaded, call google vision api
     if fname_json not in os.listdir(file_dir):
         gcv_result(file_dir + '/' + fname_proc + file_ext,
                    api_key=app_config['API_KEY'])
+
+
     # analyze json to get info of image
     res = json_result(fname_json, id_rule_dict['UA'], file_dir)
+
+
+    # res['ID']: (score, ID) # get ID by res['ID'][1]
     res['ID'] = res['ID'][1] if res['ID'] != '' else ''
     res['Filename'] = fname_proc
     res_li.append(res)
     time.sleep(0.01)
     print('Finished!', fname_proc)
     cnt += 1
-#    if cnt > 10:
-#        break
-#%%
+
+
 res_df = pd.DataFrame(res_li)
 # save result to excel
 #res_df.to_excel('json_anal_result.xlsx')
